5ada66bd910ad2d480d68699bed4a9a15af3923e
YaroslavSulyma <yaryksulima@gmail.com>
2021-03-04 10:10:39 +0200

    *added bodyparser *added footer and header

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
+const bodyParser = require('body-parser');
 
 app.set('views', __dirname + '/views');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 /*app.get('*', function (req, res) {
     res.send('Hello World!');
@@ -20,6 +22,10 @@ app.set('views', __dirname + '/views');
 
 app.get('/', function (req, res) {
     res.render('index', {user: 'Vasya'});
+});
+
+app.post('/login',function(req,res){
+    res.render('index',{user: req.body.user});
 });
 
 app.listen(3000, function () {
